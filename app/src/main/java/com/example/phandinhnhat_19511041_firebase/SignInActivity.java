3f948415e666eeeb4b289a53b306a760d19d5b3b
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-04-19 15:30:56 +0700

    update project

--- app/src/main/java/com/example/phandinhnhat_19511041_firebase/SignInActivity.java
+++ app/src/main/java/com/example/phandinhnhat_19511041_firebase/SignInActivity.java
@@ -1,11 +1,20 @@
 package com.example.phandinhnhat_19511041_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,21 +30,47 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        mAuth = FirebaseAuth.getInstance();
+//        mAuth = FirebaseAuth.getInstance();
 
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(email, password);
+            }
+        });
 
     }
 
-    @Override
-    protected void onStart() {
+/*    @Override
+    public void onStart() {
         super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-//            reload();
+            reload();
         }
+    }*/
+
+    protected void login(String email,String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Context context = getApplicationContext();
+                        int duration = Toast.LENGTH_SHORT;
+                        if(task.isSuccessful()) {
+                            Toast.makeText(context, "Dang Nhap thanh cong", duration).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        }else {
+                            Toast.makeText(context, "Tai khoan khong hop le", duration).show();
+                        }
+                    }
+                });
     }
 }
